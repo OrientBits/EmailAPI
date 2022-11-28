ac3a2c9796b9f8d925488dc5bc66f3d1c1b6b765
Ramshek <ramshekrana@gmail.com>
2022-11-28 20:45:02 +0530

    Initial commit

---
+++ src/main/java/com/emailapi/controller/EmailController.java
@@ -0,0 +1,13 @@
+package com.emailapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @GetMapping("/welcome")
+    public String welcome(){ 
+        return "Hello this is smy email api";
+    }
+}
