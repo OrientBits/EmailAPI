7cd8fd196cd4604cc2d7a9c5a00c86c15c1c8ff2
Ramshek <ramshekrana@gmail.com>
2022-12-03 18:47:55 +0530

    Email API completed....

--- src/main/java/com/emailapi/controller/EmailController.java
+++ src/main/java/com/emailapi/controller/EmailController.java
@@ -1,13 +1,42 @@
 package com.emailapi.controller;
 
+import com.emailapi.model.EmailRequest;
+import com.emailapi.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
 
+
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping("/welcome")
     public String welcome(){ 
         return "Hello this is smy email api";
     }
+
+
+    // api to send email
+    @PostMapping("/sendemail")
+    public ResponseEntity<?> sendEmail(@RequestBody EmailRequest request){
+        //this.emailService.sendEmail("")
+        System.out.println("Request "+request);
+        Boolean aBoolean = this.emailService.sendEmail(request.getSubject(), request.getMessage(), request.getTo());
+
+        if (aBoolean) {
+            return ResponseEntity.ok("Email is send successfully...");
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email not sent...");
+        }
+    }
+
+
+
 }
--- src/main/java/com/emailapi/service/EmailService.java
+++ src/main/java/com/emailapi/service/EmailService.java
@@ -1,5 +1,68 @@
 package com.emailapi.service;
 
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Properties;
+
+@Service
 public class EmailService {
+
+    public Boolean sendEmail(String subject, String message, String to) {
+
+        String from = "ramshek5298@gmail.com";
+
+        //Variable for gmail
+        String host = "smtp.gmail.com";
+
+        //get the system properties
+        Properties properties = System.getProperties();
+        System.out.println("PROPERTIES " + properties);
+
+        //setting important information to properties object
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        //Step 1: to get the session object..
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ramshek5298@gmail.com", "yzgvhkhnnuroyprf");
+            }
+        });
+        session.setDebug(true);
+
+        //Step 2 : compose the message [text,multi media]
+        MimeMessage m = new MimeMessage(session);
+
+        try {
+            //from email
+            m.setFrom(from);
+            //adding recipient to message
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            //adding subject to message
+            m.setSubject(subject);
+            m.setText(message);
+
+
+            //send
+            //Step 3 : send the message using Transport class
+            Transport.send(m);
+            System.out.println("Sent success...................");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
 
 }
